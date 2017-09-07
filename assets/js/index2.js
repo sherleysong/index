e9c990b9b31b2a7a3d93229592e013f03506e86b
sherleysong <sherleysong@126.com>
2017-09-08 00:27:44 +0800

    webpack dist upload

---
+++ assets/js/index2.js
@@ -0,0 +1,19 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+//require('../lib/css/mui.min.css');
+//require('../lib/js/mui.min.js');
+__webpack_require__(0);
+
+console.log("22222");
+
+/***/ })
+],[1]);
+//# sourceMappingURL=../maps/js/index2.js.map
